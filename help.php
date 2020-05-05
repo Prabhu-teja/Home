aa301e435cc9a2bbfef122da26136c74ca180339
Prabhu_teja <53848058+Prabhu-teja@users.noreply.github.com>
2020-05-05 09:45:40 +0530

    Add files via upload

--- help.php
+++ help.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="shortcut icon" href="/Images/programmer.jpg">
+    <link rel="shortcut icon" href="Images/programmer.jpg">
     <title>WebHunter Help Page</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"><link rel="stylesheet" href="Styles/style.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
@@ -11,7 +11,7 @@
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
     <style>
         body{
-            background-image: url("/Images/2.jpg");
+            background-image: url("Images/2.jpg");
             background-size: 100% 100%;
             background-repeat: no-repeat;
             background-attachment: fixed;
