733a3259e7ade57fe5b6420a1338d406d8ae959b
Prabhu_teja <53848058+Prabhu-teja@users.noreply.github.com>
2020-05-15 09:29:12 +0530

    Update anonymous.js

--- Scripts/anonymous.js
+++ Scripts/anonymous.js
@@ -1,6 +1,5 @@
 function call(){
     var text=document.getElementById("speak").value;
-    alert(text);
     x=speechSynthesis;
     x.speak(new SpeechSynthesisUtterance());
     x.speak(new SpeechSynthesisUtterance(text));
